ab05c6541715fe56d30439617293d47bcab4518d
EDNILSON-NASCIMENTO <ednilson.nascimento@mail.com>
2021-03-24 16:45:16 -0300

    primeiro commit turma 19 agora em java

---
+++ java/AULAS/src/Exemplo/OiMundo.java
@@ -0,0 +1,14 @@
+package Exemplo;
+
+public class OiMundo {
+	
+	public static void main(String[] args) {
+		
+		//escreva
+		System.out.print("Oi turma 19 generation!!!\n");
+		System.out.print("Vamos programa em Java!!!");
+		
+		
+	}
+
+}
